a9d2c3b82ade3a658ba9dbd01f4c3f2a91cfc4e2
Richard Jamieson <richard.jamieson@sscsuk.net>
2019-06-20 23:55:33 +0100

    cosmetic

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 docker volume create scs_coredns_conf
+
 docker run -d --name=scs_coredns --network=jamnet --hostname=coredns --restart=always  \
 -p 53:53/udp \
 --volume scs_coredns_conf:/mnt/coredns \
