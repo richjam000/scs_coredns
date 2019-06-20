f936df90489ad3502fbef3c3d0833f322bf147fe
Richard Jamieson <richard.jamieson@sscsuk.net>
2019-06-20 22:38:14 +0100

    fixes

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,4 +1,5 @@
 docker volume create scs_coredns_conf
-docker run -d --name=scs_coredns --network=jamnet --hostname=coredns \
---volume scs_coredns_conf:/mnt/coredns
-localhost:5000/scsuk.net/scs_coredns:1.0
+docker run -d --name=scs_coredns --network=jamnet --hostname=coredns --restart=always  \
+-p 53:53 \
+--volume scs_coredns_conf:/mnt/coredns \
+localhost:5000/scsuk.net/scs_coredns:1.0 -conf /mnt/coredns/Corefile
